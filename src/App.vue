3239987ddedbc43196de573d8ff6f1330f6d7f3b
Daniela01plwqpo <Danielavalerionu@gmail.com>
2025-09-25 21:23:10 -0400

    Primer commit frontend

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Productos />
+  </div>
+</template>
+
+<script setup>
+import Productos from './components/Productos.vue';
+</script>
+
---
+++ src/components/productos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Lista de Productos</h1>
+    <ul>
+      <li v-for="producto in productos" :key="producto.id_producto">
+        {{ producto.nombre }} - ${{ producto.precio }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const productos = ref([]);
+
+onMounted(async () => {
+  try {
+    const res = await axios.get('http://localhost:3000/api/productos');
+    productos.value = res.data;
+  } catch (err) {
+    console.error(err);
+  }
+});
+</script>
+
+
+
